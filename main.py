a80fc6f8c6784cf34916b2b059a88d36fc8d3631
Alex <78511004+alexeysherbakov@users.noreply.github.com>
2023-06-07 02:53:01 +0300

    much better code
    The code structure has been completely overhauled for increased efficiency and reliability. Also, implemented a new feature where .txt files are saved in a more readable format, enhancing the user's experience.

--- main.py
+++ main.py
@@ -1,42 +1,73 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 from youtube_transcript_api.formatters import TextFormatter
+import urllib
+import urllib.request
 from urllib.parse import urlparse, parse_qs, urlsplit
 import requests
+import nltk
+import json
 
-def getvideo():
-    global video_id
-    url=input('enter youtube url: ')
-    print('\n===\nplease, wait...\n===\n')
-    if '=' in url:
-        parsed_url = urlparse(url)
-        params = parse_qs(parsed_url.query)
-        video_id = params['v'][0]
-    else:
-        parsed = urlsplit(url)
-        video_id=parsed.path.replace('/','')
+# nltk.download('punkt')
 
-def getsubs():
-    global subs
-    gurl=YouTubeTranscriptApi.get_transcript(video_id)
-    formatter = TextFormatter()
-    subs=formatter.format_transcript(gurl)
+class GetYouTubeSubs:
+    def __init__(self):
+        self.video_id = None
+        self.video_title = None
+        self.subs = None
+        self.response = None
+    
+    def getvideo(self):
+        url=input('enter youtube url: ')
+        print('\n===\nplease, wait...\n===\n')
+        if '=' in url:
+            parsed_url = urlparse(url)
+            params = parse_qs(parsed_url.query)
+            self.video_id = params['v'][0]
+        else:
+            parsed = urlsplit(url)
+            self.video_id=parsed.path.replace('/','')
+    
+    def gettitle(self):
+        params = {"format": "json", "url": "https://www.youtube.com/watch?v=%s" % self.video_id}
+        url = "https://www.youtube.com/oembed"
+        query_string = urllib.parse.urlencode(params)
+        url = url + "?" + query_string
 
-def punctuation():
-    global response
-    data = {'text': f'{subs}'}
-    response = requests.post('http://bark.phon.ioc.ee/punctuator', data=data)
-    print(response.text)
+        with urllib.request.urlopen(url) as response:
+            response_text = response.read()
+            data = json.loads(response_text.decode())
+            self.video_title=data['title']
 
-def savetext():
-    with open ('output.txt', 'w') as f:
-        f.write(response.text)
-    print('\n===\ndone\n===')
+    def getsubs(self):
+        gurl=YouTubeTranscriptApi.get_transcript(self.video_id)
+        formatter = TextFormatter()
+        self.subs=formatter.format_transcript(gurl)
+
+    def punctuation(self):
+        data = {'text': self.subs}
+        try:
+            self.response = requests.post('http://bark.phon.ioc.ee/punctuator', data=data)
+        except Exception as e:
+            print(e)
+        for r in ['[  ]', '[,', '. ]', '[ Music ]']:
+            self.response.text.replace(r, '')
+
+    def savetext(self):
+        sentences = nltk.tokenize.sent_tokenize(self.response.text)
+        with open(f'{self.video_title}.txt', 'w+') as file:
+            for i in range(0, len(sentences), 5):
+                file.write(' '.join(sentences[i:i+5]) + '\n' + '\n')
+            file.seek(0)
+            print(file.read())
+        print('\n===\ndone\n===\n')
     
 def main():
-    getvideo()
-    getsubs()
-    punctuation()
-    savetext()
+    gyts=GetYouTubeSubs()
+    gyts.getvideo()
+    gyts.gettitle()
+    gyts.getsubs()
+    gyts.punctuation()
+    gyts.savetext()
 
 if __name__ == '__main__':
     main()
